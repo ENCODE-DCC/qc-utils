458a4cff7ccbba4cd130e055569c38c49206ae68
ottojolanki <ojolanki@stanford.edu>
2019-03-15 13:47:39 -0700

    parsers to different file

--- qc_utils/qcmetric.py
+++ qc_utils/qcmetric.py
@@ -2,23 +2,6 @@ from bisect import insort
 from collections import OrderedDict
 
 
-# Parser functions
-def parse_starlog(path_to_log):
-    """Parse star logfile into a dict.
-    Args:
-        path_to_log: filepath containing the star run log
-    Returns:
-        dict that contains metrics from star log
-    """
-    qc_dict = {}
-    with open(path_to_log) as f:
-        for line in f:
-            if '|' in line:
-                tokens = line.split('|')
-                qc_dict[tokens[0].strip()] = tokens[1].strip()
-    return qc_dict
-
-
 class QCMetric(object):
     """Container that holds the qc metric as OrderedDict (sorted by keys of
     the input dict) and the "master" key (name) of the said qc metric. Can be
